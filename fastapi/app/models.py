ef4b5693eaa14b1fc920628891c060e73a4af06a
amila7 <dorape1017@gmail.com>
2025-01-27 17:18:15 +0900

    still vte

--- fastapi/app/models.py
+++ fastapi/app/models.py
@@ -11,7 +11,7 @@ class Post(Base):
     published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
 
-    owner_id = Column(Integer,ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner_id = Column(Integer,ForeignKey("users.id", ondelete="CASCADE"), nullable=False,)
 
 
 class User(Base):
